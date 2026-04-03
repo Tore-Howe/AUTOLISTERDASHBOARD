a03878b99f0231d35d0222049ea44875d1d5009d
Tore-Howe <Tore.howe@outlook.com>
2026-04-03 13:58:18 -0400

    Add files via upload

--- autolist-dashboard/next.config.js
+++ autolist-dashboard/next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': ['**/@swc/core*', '**/esbuild*'],
+    },
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images2.autotrader.com' },
-      { protocol: 'https', hostname: 'atcmedia.autos' },
     ],
   },
 }
